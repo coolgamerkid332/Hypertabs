717ee566c8d059afaedbe641d6b3d9729cedcc8d
coolgamerkid332 <116192687+coolgamerkid332@users.noreply.github.com>
2023-06-02 09:26:45 -0500

    changebare

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
 	prefix: '/uv/service/',
-	bare: `https://hydrometeorology.newnotionscience.com/bare/`,
+	bare: `https://bless.kanda.ru/bare/`,
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
 	handler: '/uv/uv.handler.js',
